30c17639b84295c4546c5bfd7e16cbcf731e4e89
Azael Rivera <azaelkiller@gmail.com>
2021-07-01 19:46:40 -0600

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,33 @@ import { TaskService } from './services/task.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   title = 'machine-learning';
-  constructor(
-    private taskService:TaskService
-  ){}
+  constructor(private taskService: TaskService) {
+    this.getAllTasks();
+  }
 
-  getAllTasks(){
-    this.taskService.getAllTasks()
-    .subscribe(todos =>{
-      console.log({todos});
-    })
+  getAllTasks() {
+    this.taskService
+      .getAllTasks({
+        Inputs: {
+          input1: {
+            ColumnNames: ['UserId', 'movieId', 'Rating', 'Timestamp'],
+            Values: [
+              ['0', '0', '0', '0'],
+              ['0', '0', '0', '0'],
+            ],
+          },
+        },
+        GlobalParameters: {},
+      })
+      .subscribe(
+        (todos) => {
+          console.log({ todos });
+        },
+        (err) => console.log(err)
+      );
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Task} from './../interfaces/task';
+import { Task } from './../interfaces/task';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  public todo:any
-  constructor(
-    private http: HttpClient
-  ) { }
-  getAllTasks(){
+  public todo: any;
+  constructor(private http: HttpClient) {}
+  getAllTasks(body: any) {
     //apikey  335B16A6qPAwCNriQ9053TgPTyIrhAFiYIclLtq0OdXBO6hXN0t4VY8/K34nrAG3D26Hqp0Uk17eGftbS3iRkw==
 
-    const path='https://ussouthcentral.services.azureml.net/workspaces/be769bc549c24feb8cc5715d5120eaa1/services/65f2625e6f5b4b35ad7f6aa692b9d873/execute?api-version=2.0&details=true';
-    return this.http.post<Task[]>((``),path);
+    const path =
+      'http://localhost:4200/api';
+    return this.http.post(path, body, {
+      headers: {
+        Authorization:
+          'Bearer 335B16A6qPAwCNriQ9053TgPTyIrhAFiYIclLtq0OdXBO6hXN0t4VY8/K34nrAG3D26Hqp0Uk17eGftbS3iRkw==',
+        'Content-Length': (JSON.stringify(body).length).toString(),
+      },
+    });
   }
 }
-
